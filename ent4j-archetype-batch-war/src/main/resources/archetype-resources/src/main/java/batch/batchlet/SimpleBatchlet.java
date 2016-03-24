432102ae2c340ffe67e9f9e87454d5cae00f205f
banz-ghb <banz.ggl@gmail.com>
2016-03-25 01:18:04 +0900

    implement ent4j-archetype-batch-war

---
+++ ent4j-archetype-batch-war/src/main/resources/archetype-resources/src/main/java/batch/batchlet/SimpleBatchlet.java
@@ -0,0 +1,26 @@
+package ${package}.batch.batchlet;
+
+import javax.batch.api.Batchlet;
+import javax.enterprise.context.Dependent;
+import javax.inject.Named;
+
+import lombok.extern.java.Log;
+
+@Dependent
+@Named
+@Log
+public class SimpleBatchlet implements Batchlet {
+
+	@Override
+	public String process() throws Exception {
+		log.info("SimpleBatchlet started...");
+		return "COMPLETED";
+	}
+
+	@Override
+	public void stop() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
